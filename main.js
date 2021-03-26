e6ba3813aebb534d6c78e544a5aa5efd3303fcfb
Aditya203204 <76596474+Aditya203204@users.noreply.github.com>
2021-03-26 17:01:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+var first="My";
+console.log(first);
+
+var second="first";
+console.log(second);
+
+var third="console";
+console.log(third);
+
+var forth="project";
+console.log(forth);
+
+var fstf=first.concat(second,third,forth);
+console.log(fstf);
+
+var length_of_fstf=fstf.length;
+console.log(length_of_fstf);
+
+var name="bus";
+console.log(nameo);
+var replace=nameo.replace(bus,car);
+console.log(replace);
+
+var cap_n="ADITYA";
+console.log(cap_n);
+var lc=cap_n.toLowerCase();
+console.log(lc);
+
+var cap_n="ADITYA";
+console.log(sm_n);
+var uc=cap_n.toUpperCase();
+console.log(uc);
+
+var sp_rt=Math.sqrt(16);
+console.log(sp_rt);
+var eg1= Math.max(15,10);
+console.log(eg1);
+var eg2=Math.min(15,10);
+console.log(eg2);
+var mpow1=Math.pow(4,2);
+console.log(mpow1);
+var mpow2=Math.pow(4,3);
+console.log(mpow2);
